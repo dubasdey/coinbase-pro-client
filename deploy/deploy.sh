81b851663335cb54bf7fd41b4f7f65edb2f110c6
dubasdey <dubasdey@gmail.com>
2019-01-21 18:02:57 +0100

    iv encoded changed

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,7 +3,7 @@ echo "B=$TRAVIS_BRANCH T=$TRAVIS_TAG RQ=$TRAVIS_PULL_REQUEST"
 if [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 	echo "Deploy $TRAVIS_TAG"
 	echo "Preparing key"
-	openssl aes-256-cbc -K $encrypted_3d7f448369a7_key -iv $encrypted_3d7f448369a7_iv -in deploy/codesigning.asc.enc -out deploy/codesigning.asc -d
+	openssl aes-256-cbc -K $encrypted_770844871d6b_key -iv $encrypted_770844871d6b_iv -in deploy/codesigning.asc.enc -out deploy/codesigning.asc -d
 	echo "Importing key"
 	gpg --fast-import deploy/codesigning.asc
 	echo "Maven deploy"
